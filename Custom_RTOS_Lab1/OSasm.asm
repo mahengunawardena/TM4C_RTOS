5ea89acda01facdcb6aef932338a35434f8e04b6
mahengunawardena <mahengunawardena@hotmail.com>
2022-08-12 18:18:34 -0400

    Update documentation on OSasm.asm file

--- Custom_RTOS_Lab1/OSasm.asm
+++ Custom_RTOS_Lab1/OSasm.asm
@@ -1,3 +1,37 @@
+// Name        : Basic Micro Controller Operating System OSasm.asm
+// Author      : Mahendra Gunawardena
+// Version     : Rev 0.01
+// Copyright   : Your copyright notice
+// Description : Demonstration of Basic Operating System Design Using both Assembly and C
+//             : on TM4C123 ARM Cortex M4 Micro Controller
+//============================================================================
+/*
+ * Basic Micro Controller Operating System main.c
+ * Implementation of Basic Operating System Design Using both Assembly and C
+*  on TM4C123 ARM Cortex M4 Micro Controller. The project access SW1 and SW2 and
+ * to active the tricolor LED. Task1 monitors SW1 and TASK2 monitors SW2. Count1 and Count2
+ * global variable are used to notify TASK3 responsible for activating the Tricolor LED
+ *
+ * Copyright Mahendra Gunawardena, Mitisa LLC
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL I
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 
         .thumb
