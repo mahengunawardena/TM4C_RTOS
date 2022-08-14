79935407f630ede71beb7297dfa690e7b1464f7d
mahengunawardena <mahengunawardena@hotmail.com>
2022-08-14 09:46:03 -0400

    Update Documentation on os.c and os.h

--- Custom_RTOS_Lab1/os.c
+++ Custom_RTOS_Lab1/os.c
@@ -1,5 +1,35 @@
-// os.c
-// Runs on TM4C123
+// Name        : Basic Micro Controller Operating System os.c
+// Author      : Mahendra Gunawardena
+// Version     : Rev 0.01
+// Copyright   : Your copyright notice
+// Description : Demonstration of Basic Operating System Design Using both Assembly and C
+//             : on TM4C123 ARM Cortex M4 Micro Controller
+//============================================================================
+/*
+ * Basic Micro Controller Operating System os.c
+ * Implementation of Basic Operating System Design Using both Assembly and C
+*  on TM4C123 ARM Cortex M4 Micro Controller.
+ *
+ * Copyright Mahendra Gunawardena, Mitisa LLC
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL I
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <stdint.h>
 #include "os.h"
@@ -10,6 +40,8 @@
 // function definitions in osasm.s
 void OS_DisableInterrupts(void); // Disable interrupts
 void OS_EnableInterrupts(void);  // Enable interrupts
+// The below 2 functions are defined in CortexM.h
+// The functions are in OSasm.asm file.
 //int32_t StartCritical(void);
 //void EndCritical(int32_t primask);
 void StartOS(void);
--- Custom_RTOS_Lab1/os.h
+++ Custom_RTOS_Lab1/os.h
@@ -1,5 +1,35 @@
-// os.h
-// Runs on TM4C123
+// Name        : Basic Micro Controller Operating System os.h
+// Author      : Mahendra Gunawardena
+// Version     : Rev 0.01
+// Copyright   : Your copyright notice
+// Description : Demonstration of Basic Operating System Design Using both Assembly and C
+//             : on TM4C123 ARM Cortex M4 Micro Controller
+//============================================================================
+/*
+ * Basic Micro Controller Operating System os.h
+ * Implementation of Basic Operating System Design Using both Assembly and C
+*  on TM4C123 ARM Cortex M4 Micro Controller.
+ *
+ * Copyright Mahendra Gunawardena, Mitisa LLC
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL I
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 
 #ifndef __OS_H
@@ -8,7 +38,7 @@
 
 // ******** OS_Init ************
 // initialize operating system, disable interrupts until OS_Launch
-// initialize OS controlled I/O: SysTick, 48 MHz crystal
+// initialize OS controlled I/O: SysTick
 // Inputs: none
 // Outputs: none
 void OS_Init(void);
